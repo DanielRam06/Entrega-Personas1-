1957ad906c86bf58134cf7b33a4a4d60a5192bc7
DanielRam06 <122307845+DanielRam06@users.noreply.github.com>
2023-02-28 19:48:56 -0600

    Add files via upload

--- src/Mian1.java
+++ src/Mian1.java
@@ -1,7 +1,4 @@
 
-
-
-
 public class Mian1 {
 	
 
@@ -10,7 +7,7 @@ public class Mian1 {
 	        for (int i = 0; i < 10; i++) {
 	            persona[i] = new Personas() {};
 	        }
-	        
+	      //contadores de los atributos  
 	        int espada = 0;
 	        int millonario = 0;
 	        int mamado = 0;
@@ -38,45 +35,3 @@ public class Mian1 {
 	    }
 	}
 
-/*
-	public static void main(String[] args) {
-	Personas[] persona = new Personas[10];
-	persona[0] = new Personas() {};
-	persona[1] = new Personas() {};
-	persona[2] = new Personas() {};
-	persona[3] = new Personas() {};
-	persona[4] = new Personas() {};
-	persona[5] = new Personas() {};
-	persona[6] = new Personas() {};
-	persona[7] = new Personas() {};
-	persona[8] = new Personas() {};
-	persona[9] = new Personas() {};
-	
-	int espada = 0;
-    int millonario = 0;
-    int mamado = 0;
-    int miedoCucarachas = 0;
-     
-    for (Personas personas : persona) {
-    	if (personas.gettieneEspada()) {
-    		espada++;	
-    	}
-    	if (personas.getesMillonario()) {
-    		millonario++;	
-    	}
-    	if (personas.getestaMamado()) {
-    		mamado++;	
-    	}
-    	if (personas.getmiedoCucarachas()) {
-    		miedoCucarachas++;	
-    	}
-    	System.out.println(personas.getNombre() + ": " + personas.gettieneEspada() + "," + personas.getesMillonario() + "," + personas.getestaMamado() + "," + personas.getmiedoCucarachas());
-    }
-    System.out.println("El Atributo Tiene Espada se repite: " + espada);
-    System.out.println("El Atributi es Millonario se repite: " + millonario);
-    System.out.println("El Atributo esta Mamado se repite: " + mamado);
-    System.out.println("El Atributo Miedo a cucarachas se repite: " + miedoCucarachas);
-
-	}
-}
-*/
